221420dc832b9489bceb50d78500ce6ed93d90ff
Andy Bettisworth <andy@accreu.com>
2019-04-26 10:30:27 -0500

    js: format App.js for readability

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,41 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const React = require('react')
+
+/**
+ * Constants
+ */
+
+const Component = React.Component
+
+/**
+ * Import component styles
+ */
+
+require('./App.scss')
+
+/**
+ * Define component
+ */
+
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       starwarsChars: []
-    };
+    }
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters('https://swapi.co/api/people/')
   }
 
   getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch(URL)
       .then(res => {
-        return res.json();
+        return res.json()
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ starwarsChars: data.results })
       })
       .catch(err => {
-        throw new Error(err);
-      });
-  };
+        throw new Error(err)
+      })
+  }
 
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+/**
+ * Export component
+ */
+
+module.exports = App
